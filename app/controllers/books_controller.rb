1636aaa4a08dceb4b1764673c89b61ade32defd0
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-08-30 14:48:48 +0000

    Initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,43 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = User.find_by(id: @book.user_id)
   end
 
   def edit
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to "/books/#{ @book.id }"
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to "/books"
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    redirect()
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = Book.where(user_id: @user.id)
+    @book = Book.new
   end
 
   def edit
