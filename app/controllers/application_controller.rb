8a727f97628b9184d99c011d4c5ffb0311525559
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-09-01 04:22:03 +0000

    [ Add]パーシャル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top,:about]
-
-  def after_sign_up_path_for(resource)
-    user_path(current_user.id)
-  end
   
   def after_sign_in_path_for(resource) 
     user_path(current_user.id)
